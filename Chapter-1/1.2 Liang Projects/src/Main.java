c7709204a20d149cc114feebc1cc6e89e6c46b5c
Nadjah Duke <nadjahduke@gmail.com>
2024-10-17 19:38:30 -0400

    Added 1.2 & 1.3 Liang Projects

---
+++ Chapter-1/1.2 Liang Projects/src/Main.java
@@ -0,0 +1,12 @@
+// Write a program tha displayes Welcome to Java 5 times
+
+public class Main {
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
