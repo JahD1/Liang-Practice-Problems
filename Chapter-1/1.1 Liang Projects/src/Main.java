7582161a66c8b44ed9f0c1db38499ff8731eca51
Nadjah Duke <nadjahduke@gmail.com>
2024-10-17 19:34:55 -0400

    Added folder 1.1 Liang Projects

---
+++ Chapter-1/1.1 Liang Projects/src/Main.java
@@ -0,0 +1,15 @@
+/*
+(Display three messages)
+Write a program that displays
+1. " Welcome to Java
+2. " Welcome to Computer Science"
+3. " Programming is fun."
+ */
+public class Main {
+    public static void main (String[] args )
+    {
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is fun.");
+    }
+}
