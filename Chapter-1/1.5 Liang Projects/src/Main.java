7360a3dd19de3f881c7c3326f3bdf67c2c40b519
Nadjah Duke <nadjahduke@gmail.com>
2024-10-18 19:21:04 -0400

    Added Liang 1.4-1.6

---
+++ Chapter-1/1.5 Liang Projects/src/Main.java
@@ -0,0 +1,13 @@
+/*
+*Write a program that displays the results of
+* (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+ */
+
+public class Main{
+    public static void main(String[] args)
+    {
+        System.out.println("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) =" +
+                (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+
+    }
+}
